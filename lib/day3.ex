24561c28f09c9c3c97414cc07c8fd9be5e731b1a
Pavel Ro <buurzx@gmail.com>
2018-12-16 13:42:14 +0300

    Day3 second task

--- lib/day3.ex
+++ lib/day3.ex
@@ -25,20 +25,18 @@ defmodule Day3 do
   ## Examples
 
       iex> claimed = Day3.claimed_inches([
-      ...>    "#1 @ 1,3: 4x4",
-      ...>    "#2 @ 3,1: 4x4",
-      ...>    "#3 @ 5,5: 2x2"
+      ...>    [1, 1, 3, 4, 4],
+      ...>    [2, 3, 1, 4, 4],
+      ...>    [3, 5, 5, 2, 2]
       ...>  ])
       iex> claimed[{4,2}]
       [2]
       iex> claimed[{4,4}]
       [2, 1]
   """
-  @spec claimed_inches([claim]) :: %{coordinate => [id]}
-  def claimed_inches(claims) do
-    Enum.reduce(claims, %{}, fn claim, acc ->
-      [id, left, top, width, height] = parse_claim(claim)
-
+  @spec claimed_inches([parsed_claim]) :: %{coordinate => [id]}
+  def claimed_inches(parsed_claims) do
+    Enum.reduce(parsed_claims, %{}, fn [id, left, top, width, height], acc ->
       Enum.reduce((left + 1)..(left + width), acc, fn x, acc ->
         Enum.reduce((top + 1)..(top + height), acc, fn y, acc ->
           Map.update(acc, {x, y}, [id], &[id | &1])
@@ -53,14 +51,43 @@ defmodule Day3 do
   ## Examples
 
       iex> Day3.overlapped_inches([
-      ...>    "#1 @ 1,3: 4x4",
-      ...>    "#2 @ 3,1: 4x4",
-      ...>    "#3 @ 5,5: 2x2"
+      ...>    [1, 1, 3, 4, 4],
+      ...>    [2, 3, 1, 4, 4],
+      ...>    [3, 5, 5, 2, 2]
       ...>  ]) |> Enum.sort()
       [{4, 4}, {4, 5}, {5, 4}, {5, 5}]
   """
   @spec overlapped_inches([claim]) :: [coordinate]
-  def overlapped_inches(claims) do
-    for {coordinate, [_, _ | _]} <- claimed_inches(claims), do: coordinate
+  def overlapped_inches(parsed_claims) do
+    for {coordinate, [_, _ | _]} <- claimed_inches(parsed_claims), do: coordinate
+  end
+
+  @doc """
+  Find non-overlappng claim
+
+  ## Examples
+
+      iex> Day3.not_overlapping_claim([
+      ...>    "#1 @ 1,3: 4x4",
+      ...>    "#2 @ 3,1: 4x4",
+      ...>    "#3 @ 5,5: 2x2"
+      ...>  ])
+      3
+  """
+  @spec not_overlapping_claim([claim]) :: id
+  def not_overlapping_claim(claims) do
+    parsed_claims = Enum.map(claims, &parse_claim/1)
+    claimed_inches = claimed_inches(parsed_claims)
+
+    [id, _, _, _, _] =
+      Enum.find(parsed_claims, fn [id, left, top, width, height] ->
+        Enum.all?((left + 1)..(left + width), fn x ->
+          Enum.all?((top + 1)..(top + height), fn y ->
+            Map.get(claimed_inches, {x, y}) == [id]
+          end)
+        end)
+      end)
+
+    id
   end
 end
